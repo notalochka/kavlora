0863697a2b6e18794e158728cdbfeaefa9458947
notalochka <nashepel2004@gmail.com>
2026-04-28 13:13:40 +0300

    deleted videos

--- src/pages/advantages.tsx
+++ src/pages/advantages.tsx
@@ -206,61 +206,7 @@ export default function AdvantagesPage() {
               aria-label={t.videoGalleryTitle}
             />
           </div>
-          <div className={styles.videoGalleryInner}>
-            <div className={styles.videoHeader}>
-              <h2 className={styles.videoGalleryTitle}>{t.videoGalleryTitle}</h2>
-              <div className={styles.videoNav}>
-                <button
-                  type="button"
-                  className={styles.videoNavBtn}
-                  onClick={() => scrollVideos("left")}
-                  disabled={!canScrollLeft}
-                  aria-label={t.prevVideosLabel}
-                >
-                  <svg
-                    aria-hidden="true"
-                    className={`${styles.videoNavIcon} ${styles.videoNavIconLeft}`}
-                    viewBox="0 0 320 512"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className={styles.videoNavBtn}
-                  onClick={() => scrollVideos("right")}
-                  disabled={!canScrollRight}
-                  aria-label={t.nextVideosLabel}
-                >
-                  <svg
-                    aria-hidden="true"
-                    className={styles.videoNavIcon}
-                    viewBox="0 0 320 512"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-            <div ref={videoTrackRef} className={styles.videoGrid}>
-              {["/technology/video1.MOV", "/technology/video2.MOV", "/technology/video3.MOV", "/technology/video4.MOV"].map(
-                (src, index) => (
-                  <figure key={src} className={styles.videoCard}>
-                    <video
-                      className={styles.videoPlayer}
-                      src={src}
-                      controls
-                      playsInline
-                      preload="metadata"
-                      aria-label={`${t.videoLabel} ${index + 1}`}
-                    />
-                  </figure>
-                )
-              )}
-            </div>
-          </div>
+          
         </section>
       </main>
 
